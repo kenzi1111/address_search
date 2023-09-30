228420d7288886d9e4f439a695d2b93e9f0915f4
kenzi1111 <88967765+kenzi1111@users.noreply.github.com>
2023-09-30 09:55:39 +0900

    Add sample.py And address_research.py

---
+++ address_search.py
@@ -0,0 +1,35 @@
+import requests
+
+# zipcode = input("郵便番号<ハイフン無し7桁>は？")
+zipcode = "0287111"
+
+response = requests.get(f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}")
+
+# print(response)
+# print(response.text)
+
+# print(response.json())
+# typeを使ってjsonを調べる
+# print(type(response.json()))
+
+# dic = response.json()
+
+# # print(dic)
+
+# # print(f"{dic['results'][0]['address1']}{dic['results'][0]['address2']}{dic['results'][0]['address3']}")
+
+# リファクタリング前
+# prefecture_name = dic["results"][0]["address1"]
+# city_name = dic["results"][0]["address2"]
+# town_name = dic["results"][0]["address3"]
+
+# リファクタリング後
+address_info = response.json()["results"][0]
+
+prefecture_name = address_info["address1"]
+city_name = address_info["address2"]
+town_name = address_info["address3"]
+
+address = f"{prefecture_name}{city_name}{town_name}"
+
+print(address)
---
+++ sample1.py
@@ -0,0 +1,7 @@
+def double(number:int) -> int:
+    # """引数の2倍の整数を返す関数""""
+
+    return number * 2
+
+print(double(3))
+print(double(9))
